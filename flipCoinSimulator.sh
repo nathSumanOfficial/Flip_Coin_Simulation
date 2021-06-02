220e13df0d87583ac03c7e881df01112e22672c2
Suman Nath <nathsuman1099@gmail.com>
2021-06-02 19:37:56 +0530

    If tied, will continue till one wins with 2 difference

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -16,6 +16,22 @@ while [ $head != 21 ] && [ $tail != 21 ]; do
 
 	if [[ $head -eq 20 ]] && [[ $tail -eq 20 ]]; then
 		flag=1
+		diff=0
+		while [ $diff != 2 ]; do
+			toss=$((RANDOM%2))
+			if [[ toss -eq 0 ]]; then
+				head=$(($head+1))
+			else
+				tail=$(($tail+1))
+			fi
+
+			if [[ $head -gt $tail ]]; then
+				diff=$(($head - $tail))
+			else
+				diff=$(($tail - $head))
+			fi
+		done
+
 		break
 	fi
 
@@ -27,6 +43,14 @@ if [[ $head -eq 21 ]] && [[ $flag -eq 0 ]]; then
 elif [[ $tail -eq 21 ]] && [[ $flag -eq 0 ]]; then
 	echo $'\n'"Tail Wins $tail times"
 	echo "Tail wins by $(($tail-$head)) tosses"
-elif [[ $flag -eq 1 ]]; then
+elif [[ $diff -eq 2 ]] && [[ $head -gt $tail ]]; then
 	echo $'\n'"Both are Tied"
+	echo "Tosses continued till the difference was $diff"
+	echo "Head won with $head tosses"
+	echo "Tail had $tail tosses"
+elif [[ $diff -eq 2 ]] && [[ $head -lt $tail ]]; then
+	echo $'\n'"Both are Tied"
+	echo "Tosses continued till the difference was $diff"
+	echo "Tail won with $tail tosses"
+	echo "Head had $head tosses"
 fi
