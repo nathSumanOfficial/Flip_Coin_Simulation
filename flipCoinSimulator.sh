4c0c4ae745e33c6ffe0e6bb3e222d3f283e4234d
Suman Nath <nathsuman1099@gmail.com>
2021-06-02 17:02:00 +0530

    Win/Tie in 21 tosses

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,19 +2,31 @@
 
 echo $'\n'Welcome to Coin Flip Simulator$'\n'
 
-heads=0
-tails=0
+head=0
+tail=0
+flag=0
 
-for (( i = 0; i < 20; i++ )); do
+while [ $head != 21 ] && [ $tail != 21 ]; do
 	toss=$((RANDOM%2))
-
-	if [[ $toss -eq 0 ]]; then
-		((++heads))
+	if [[ toss -eq 0 ]]; then
+		head=$(($head+1))
 	else
-		((++tails))
+		tail=$(($tail+1))
+	fi
+
+	if [[ $head -eq 20 ]] && [[ $tail -eq 20 ]]; then
+		flag=1
+		break
 	fi
 
 done
 
-echo Head Won $heads times
-echo Tail Won $tails times
+if [[ $head -eq 21 ]] && [[ $flag -eq 0 ]]; then
+	echo $'\n'"Head Wins $head times"
+	echo "Head wins by $(($head-$tail)) tosses"
+elif [[ $tail -eq 21 ]] && [[ $flag -eq 0 ]]; then
+	echo $'\n'"Tail Wins $tail times"
+	echo "Tail wins by $(($tail-$head)) tosses"
+elif [[ $flag -eq 1 ]]; then
+	echo $'\n'"Both are Tied"
+fi
